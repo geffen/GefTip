33118dbb0c645b04709ae31cb9c2940e179bdae7
Joseph Van Geffen <geffen@gmail.com>
2015-10-25 17:33:00 -0700

    Identity Changes

--- GefTip/ViewController.swift
+++ GefTip/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipChoice: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        //Open Keyboard immediately - UX doesn't need to wait
+        billField.becomeFirstResponder()
     }
 
+    @IBAction func billChange(sender: AnyObject) {
+        var tipPercentages = [0.18,0.2,0.22]
+        var tipChosen = tipPercentages[tipChoice.selectedSegmentIndex]
+        
+        var billAmount = NSString(string:billField.text!).doubleValue
+        var tip = billAmount * tipChosen
+        var total = billAmount + tipChosen
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+     
+        tipLabel.text = String (format: "$%.2f", tip)
+        totalLabel.text = String (format: "$%.2f", total)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
